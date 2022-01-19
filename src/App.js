48bfcef1c23775c61491e260f87887df0b711c9f
bogvoicescu <bog.voicescu@gmail.com>
2022-01-19 19:56:07 +0200

    rebuilt with react

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, {useState} from 'react';
+import MainMenu from "./Components/MainMenu";
+import Quiz from "./Components/Quiz";
+import EndScreen from "./Components/EndScreen";
+import { QuizContext } from "./Helpers/Context";
+
+function App() {
+  const [gameState, setGameState] = useState("menu");
+
+  return (
+
+    <div className="App">
+      <hi>Quiz App</hi>
+      <QuizContext.Provider value={{gameState, setGameState}}>
+        {gameState === "menu" && <MainMenu />}
+        {gameState === "quiz" && <Quiz />}
+        {gameState === "endScreen" && <EndScreen />}
+      </QuizContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+        prompt: "Cât face 1 + 3?",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "C",
+    },
+    {
+        prompt: "A fost odată ca ...",
+        optionA: "Niciodată",
+        optionB: "Mine",
+        optionC: "Toți",
+        optionD: "Elefant",
+        answer: "A",
+    },
+    {
+        prompt: "Ce manâncă șoarecii?",
+        optionA: "Brânză",
+        optionB: "Porumb",
+        optionC: "Oameni",
+        optionD: "Ce vor ei",
+        answer: "D",
+    },
+    {
+        prompt: "Care este culoarea mea preferată?",
+        optionA: "Roșu",
+        optionB: "Verde",
+        optionC: "Albastru",
+        optionD: "Portocaliu",
+        answer: "B",
+    }
+]
